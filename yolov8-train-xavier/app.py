efd02b2a6163592236e30f00852fc6e99958e00e
impelfin <impelfin@gmail.com>
2024-07-25 17:51:13 +0900

    .

--- yolov8-train-xavier/app.py
+++ yolov8-train-xavier/app.py
@@ -1,13 +1,19 @@
 from ultralytics import YOLO
 import torch
+from torch.cuda.amp import GradScaler, autocast
 
+# GPU 사용 설정
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+# 모델 초기화
 model = YOLO('yolov8n.pt').to(device)
 
+# GradScaler 초기화
+scaler = GradScaler()
+
 # 학습 설정
-data_path = 'snack.yaml'  
-epochs = 1 
+data_path = 'snack.yaml' 
+epochs = 50 
 batch_size = 32  
 img_size = 640  
 
@@ -16,11 +22,12 @@ model.train(
     data=data_path, 
     epochs=epochs, 
     batch=batch_size, 
-    project=".",  
+    project=".", 
     name="result",  
     exist_ok=True,  
     device=device,
-    imgsz=img_size  
+    imgsz=img_size,  
+    amp=True  
 )
 
 print("학습 완료")
