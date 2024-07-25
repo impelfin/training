1074c2410cc01cda1471114739ae2cfed38aa73a
JunHyeon Mun <impelfin@gmail.com>
2024-07-25 21:39:35 +0900

    up

--- yolov8-sauron/app.py
+++ yolov8-sauron/app.py
@@ -7,7 +7,7 @@ model = YOLO('best.pt')
 device = torch.device("mps")
 
 model.predict(
-   source='./input_data/in1.png',
+   source='./input/in1.png',
    conf=0.25,
    save=True,
    project=".",
